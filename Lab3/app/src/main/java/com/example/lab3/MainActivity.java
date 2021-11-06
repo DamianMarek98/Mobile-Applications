9a287429a2059774bd65e2086e489d8725ddb55a
Damian Marek <zmdddamian@gmail.com>
2021-11-06 22:16:14 +0100

    Lab3 finished tasks

--- Lab3/app/src/main/java/com/example/lab3/MainActivity.java
+++ Lab3/app/src/main/java/com/example/lab3/MainActivity.java
@@ -7,11 +7,13 @@ import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.function.Consumer;
@@ -43,25 +45,47 @@ public class MainActivity extends AppCompatActivity {
                         Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        lm.requestLocationUpdates(
-                LocationManager.GPS_PROVIDER, 0, 0, listener);
+        registerProcessLocation();
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_main);
-        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
-        tv = (TextView) findViewById(R.id.loctv);
-        listener = new ProcessLocation(locationText -> {
-            tv.setText(locationText);
-        });
+        registerProcessLocation();
         if (ActivityCompat.checkSelfPermission(
                 this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
             return;
         }
         registerListener();
+    }
+
+    private void registerProcessLocation() {
+        if (lm == null) {
+            lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+            tv = findViewById(R.id.loctv);
+            listener = new ProcessLocation(locationText -> {
+                tv.setText(locationText);
+            });
+            addButtonCallback();
+        }
+    }
+
+    private void addButtonCallback() {
+        TextView latTv = findViewById(R.id.lat);
+        TextView lonTv = findViewById(R.id.lon);
+        findViewById(R.id.addLocButton).setOnClickListener(v -> {
+            double lat = Double.parseDouble(latTv.getText().toString());
+            double lon = Double.parseDouble(lonTv.getText().toString());
+            Location location = new Location("");
+            location.setLatitude(lat);
+            location.setLongitude(lon);
+            listener.locationConsumer.accept(location);
+            latTv.setText(null);
+            lonTv.setText(null);
+        });
     }
 
 }
--- Lab3/app/src/main/java/com/example/lab3/ProcessLocation.java
+++ Lab3/app/src/main/java/com/example/lab3/ProcessLocation.java
@@ -5,16 +5,24 @@ import android.location.LocationListener;
 
 import androidx.annotation.NonNull;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class ProcessLocation implements LocationListener {
 
     private Location prevLocation = null;
     private final Consumer<String> locationTextViewFiller;
+    public final Consumer<Location> locationConsumer;
+    private final List<Location> locations = new ArrayList<>();
+    private Location closestLocation;
 
     public ProcessLocation(Consumer<String> locationTextViewFiller) {
         this.locationTextViewFiller = locationTextViewFiller;
+        locationConsumer = locations::add;
     }
+
 
 
     @Override
@@ -28,9 +36,36 @@ public class ProcessLocation implements LocationListener {
         if (prevLocation != null) {
             float bearing = prevLocation.bearingTo(location);
             float distance = prevLocation.distanceTo(location);
-            info += " " + distance + " " + bearing;
+            info += "Dystans: " + distance + "m, kierunek:" + bearing + "\n";
+            info += "Prędkość: " + location.getSpeed() + "m/s\n";
+            info += "Akt. pozycja: lon:" + location.getLongitude() + ", lat: " + location.getLatitude() + "\n";
+            info += "Pop. pozycja: lon:" + prevLocation.getLongitude() + ", lat: " + prevLocation.getLatitude() + "\n";
+            try {
+                info += addClosestPointInfo(location);
+            } catch (Exception e) {
+                info += "Brak najbliższej lokalizacji!";
+            }
         }
         locationTextViewFiller.accept(info);
         prevLocation = location;
+    }
+
+    private String addClosestPointInfo(Location location) throws Exception {
+        if (locations.isEmpty()) {
+            return "Brak najbliższej lokalizacji!";
+        }
+
+        Location closestLocation = locations.stream()
+                .min(Comparator.comparing(l -> l.distanceTo(location)))
+                .orElseThrow(Exception::new);
+
+        if (this.closestLocation == null) {
+            this.closestLocation = closestLocation;
+        } else if (!this.closestLocation.equals(closestLocation)) {
+            this.closestLocation = closestLocation;
+        }
+        this.closestLocation = closestLocation;
+        return "Najbliższy pkt: dystans: " + location.distanceTo(this.closestLocation) / 1000 + "km, kierunek:" +
+                location.bearingTo(this.closestLocation) + "\n";
     }
 }
